6d33c7eea902ce8cefd2fd0b7a763e61ea9f6653
adnan <sirgeant10@gmail.com>
2024-12-30 18:24:03 +0600

    Add mencategory

--- src/pages/PopularMenu.jsx
+++ src/pages/PopularMenu.jsx
@@ -1,27 +1,15 @@
-import { useEffect, useState } from "react";
 import SectionTitle from "../components/SectionTitle";
-import MenuItem from "./Shared/MenuItem";
+import useMenu from "../hooks/useMenu";
+import MenuCategory from "./MenuCategory";
 
 const PopularMenu = () => {
-  const [menu, setMenu] = useState([]);
-
-  useEffect(() => {
-    fetch("menu.json")
-      .then((res) => res.json())
-      .then((data) => {
-        const popularItems = data.filter((item) => item.category === "popular");
-        setMenu(popularItems);
-      });
-  }, []);
+  const [menu] = useMenu();
+  const popular = menu.filter((item) => item.category === "popular");
 
   return (
     <section className="mb-12">
       <SectionTitle heading={"From Our Menu"} subHeading={"Popular Items"} />
-      <div className="grid md:grid-cols-2 gap-10">
-        {menu.map((item) => (
-          <MenuItem key={item.id} item={item} />
-        ))}
-      </div>
+      <MenuCategory items={popular} />
     </section>
   );
 };
