f3dd5b0c52bdf1094825aa8ae5cbdab429fabb85
adnan <sirgeant10@gmail.com>
2024-12-30 18:29:10 +0600

    Add orders link

--- src/pages/Shared/Navbar.jsx
+++ src/pages/Shared/Navbar.jsx
@@ -10,8 +10,9 @@ const Navbar = () => {
         <Link to={"/menu"}>Our Menu</Link>
       </li>
       <li>
-        <a>Item 3</a>
+        <Link to={"/order"}>Order Food</Link>
       </li>
+      
     </>
   );
   return (
