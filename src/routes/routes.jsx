e8b94ca2a0d5f412184ad541c7986177824242da
adnan <sirgeant10@gmail.com>
2024-12-30 18:28:54 +0600

    Add order page

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../pages/Home";
 import Menu from "../pages/Menu";
+import Order from "../pages/Order";
 
 const router = createBrowserRouter([
   {
@@ -16,6 +17,7 @@ const router = createBrowserRouter([
         path: "/menu",
         element: <Menu />,
       },
+      { path: "/order", element: <Order /> },
     ],
   },
 ]);
