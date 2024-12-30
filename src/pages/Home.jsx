1d78fb72e0ecb82a0d8afaccdb9a8a9a78c43614
adnan <sirgeant10@gmail.com>
2024-12-30 14:04:31 +0600

    Add testimonials to home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import Banner from "./Banner";
 import Category from "./Category";
 import Featured from "./Featured/Featured";
 import PopularMenu from "./PopularMenu";
+import Testimonials from "./Testimonials";
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
       <Category />
       <PopularMenu />
       <Featured />
+      <Testimonials />
     </div>
   );
 };
