1dd0270d631465495a7e0448653faf9e653c565f
adnan <sirgeant10@gmail.com>
2024-12-31 22:44:43 +0600

    Add btn class to link

--- src/pages/MenuCategory.jsx
+++ src/pages/MenuCategory.jsx
@@ -1,6 +1,7 @@
 import PropTypes from "prop-types";
 import MenuItem from "./Shared/MenuItem";
 import Cover from "./Shared/Cover";
+import { Link } from "react-router-dom";
 
 const MenuCategory = ({ items, title, coverImg }) => {
   return (
@@ -8,9 +9,15 @@ const MenuCategory = ({ items, title, coverImg }) => {
       {title && <Cover img={coverImg} title={title} />}
       <div className="grid md:grid-cols-2 gap-10 my-16">
         {items.map((item) => (
-          <MenuItem key={item.id} item={item} />
+          <MenuItem key={item._id} item={item} />
         ))}
       </div>
+      <Link
+        to={`/order/${title}`}
+        className="btn btn-outline border-0 border-b-4 mt-4"
+      >
+        Order Now
+      </Link>
     </div>
   );
 };
