34923ce42c13d2e60e45ca34540608e139a59c52
adnan <sirgeant10@gmail.com>
2025-01-02 14:14:24 +0600

    Add routes

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -3,6 +3,10 @@ import Main from "../Layout/Main";
 import Home from "../pages/Home";
 import Menu from "../pages/Menu";
 import Order from "../pages/Order";
+import Login from "../pages/Login/Login";
+import SignUp from "../pages/SignUp/SignUp";
+import PrivateRoute from "./PrivateRoute";
+import Secret from "../pages/Shared/Secret";
 
 const router = createBrowserRouter([
   {
@@ -18,6 +22,16 @@ const router = createBrowserRouter([
         element: <Menu />,
       },
       { path: "/order/:category", element: <Order /> },
+      { path: "/login", element: <Login /> },
+      { path: "/signup", element: <SignUp /> },
+      {
+        path: "secret",
+        element: (
+          <PrivateRoute>
+            <Secret />
+          </PrivateRoute>
+        ),
+      },
     ],
   },
 ]);
